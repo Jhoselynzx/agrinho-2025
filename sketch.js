e477ecf19bc1efb3317068dd28f50d7502523b46
Jhoselynzx <jhoselyn.mollo@escola.pr.gov.br>
2025-06-17 08:41:20 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(800, 800);
+}
+
+function draw() {
+  background("#4EA1E2");
+  fill(100,300,500);
+  //text("🌳",0,100,190);
+  text("🌄",450,505,0);
+  
+  text("🏙",-23,505,8);
+  textSize(350);
+  rect(400,0,5,800);
+  
+}
